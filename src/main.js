7100c6e78a081109472742e239d276d6bd78c16a
Addulahad <abdulahadikramov1999@gmail.com>
2023-10-04 06:23:44 +0500

    add pagejson

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+// import { store } from 'vuex'
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App)
 
 app.use(router)
+
+// app.use(store)
 
 app.mount('#app')
